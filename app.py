0b0ae0ac700b14475fd6852f70e6c7b62aa31a02
shindevpbhavitvp-crypto <shindevpbhavitvp@gmail.com>
2026-06-19 10:59:09 +0530

    Added login API with JWT authentication

--- app.py
+++ app.py
@@ -2,11 +2,14 @@ from flask import Flask
 from config.db import db
 from models.user import User
 from routes.auth_routes import auth_bp
+from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///noticeboard.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = 'myjwtsecret'
+jwt = JWTManager(app)
 
 db.init_app(app)
 app.register_blueprint(auth_bp)
--- controllers/auth_controller.py
+++ controllers/auth_controller.py
@@ -1,6 +1,7 @@
 from flask import request, jsonify
 from models.user import User
 from config.db import db
+from flask_jwt_extended import create_access_token
 
 def register():
     data = request.get_json()
@@ -10,6 +11,26 @@ def register():
         email=data["email"],
         password=data["password"]
     )
+
+    
+
+def login():
+    data = request.get_json()
+
+    user = User.query.filter_by(email=data["email"]).first()
+
+    if not user:
+        return jsonify({"message": "User not found"}), 404
+
+    if user.password != data["password"]:
+        return jsonify({"message": "Invalid password"}), 401
+
+    token = create_access_token(identity=user.id)
+
+    return jsonify({
+        "message": "Login successful",
+        "token": token
+    })
 
     db.session.add(user)
     db.session.commit()
